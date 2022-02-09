82621fac07bc5c017e1c056d9712040ac014253e
saman.pirooz@live.com <saman.pirooz@live.com>
2022-02-10 03:09:02 +0330

    Settings Page Added

---
+++ TakeOnThis/TakeOnThis/Views/SettingsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using TakeOnThis.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TakeOnThis.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        SettingsViewModel vm;
+        SettingsViewModel VM
+        {
+            get => vm ?? (vm = (SettingsViewModel)BindingContext);
+        }
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
