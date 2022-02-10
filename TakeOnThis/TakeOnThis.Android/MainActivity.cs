139cec1ccc62e3f2d4916a6e1f1a9a6847660015
saman.pirooz@live.com <saman.pirooz@live.com>
2022-02-10 15:09:09 +0330

    remove unused nuget packages from Droid

--- TakeOnThis/TakeOnThis.Android/MainActivity.cs
+++ TakeOnThis/TakeOnThis.Android/MainActivity.cs
@@ -14,12 +14,12 @@ namespace TakeOnThis.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
             
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
-            ImageCircle.Forms.Plugin.Droid.ImageCircleRenderer.Init();
+            //global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+            //ImageCircle.Forms.Plugin.Droid.ImageCircleRenderer.Init();
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
